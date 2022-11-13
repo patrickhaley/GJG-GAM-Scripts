f20dce52ea6794adb53e5d9c3159c7dea5ac3092
Patrick Haley <patrickhaley18@gmail.com>
2022-11-12 21:54:49 -0600

    Update gam_setup.sh

--- setup/gam_setup.sh
+++ setup/gam_setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# install GAMADC-XTD3 (https://github.com/taers232c/GAMADV-XTD3)
-cd ../.. &&
-<(curl -s -S -L https://raw.githubusercontent.com/taers232c/GAMADV-XTD3/master/src/gam-install.sh) &&
-
 # setup config files
-mkdir GAMWork/au &&
-mkdir GAMWork/nz &&
-mkdir GAMWork/us
+mkdir ../GAMWork/au &&
+mkdir ../GAMWork/nz &&
+mkdir ../GAMWork/us
