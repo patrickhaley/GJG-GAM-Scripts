bf9d7c8487b311f09e6f784dee474541376c64ff
Patrick Haley <patrickhaley18@gmail.com>
2022-11-12 21:31:23 -0600

    Add cd to user directory before GAM install

--- bin/gam_setup.sh
+++ bin/gam_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # install GAMADC-XTD3 (https://github.com/taers232c/GAMADV-XTD3)
+cd ../.. &&
 <(curl -s -S -L https://raw.githubusercontent.com/taers232c/GAMADV-XTD3/master/src/gam-install.sh) &&
 
 # setup config files
